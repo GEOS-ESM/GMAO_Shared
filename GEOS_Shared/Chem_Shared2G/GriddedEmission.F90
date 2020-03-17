69a8f633208ba1d793cffe0650f73d139604f327
Elliot Sherman <elliot.m.sherman@nasa.gov>
2020-03-17 11:57:35 -0400

    zero-diff dust gridded emissions. Working point emissons

--- GEOS_Shared/Chem_Shared2G/GriddedEmission.F90
+++ GEOS_Shared/Chem_Shared2G/GriddedEmission.F90
@@ -23,7 +23,7 @@
 ! !PUBLIC MEMBER FUNCTIONS:
 !
    public DustEmissionGOCART2G
-
+   public DistributePointEmission
 
   real, parameter :: OCEAN=0.0, LAND = 1.0, SEA_ICE = 2.0
 
@@ -41,72 +41,71 @@
 CONTAINS
 
 
+!------------------------------------------------------------------------
+!BOP
+! !IROUTINE: DustEmissionGOCART2G
 
-   subroutine DustEmissionGOCART2G( i1, i2, j1, j2, n_bins, radius, &
-                                  fraclake, gwettop, oro, u10m, v10m, &
-                                  Ch_DU, sfrac, du_src, grav, &
-                                  emissions, rc )
+   subroutine DustEmissionGOCART2G(radius, fraclake, gwettop, oro, u10m, &
+                                   v10m, Ch_DU, sfrac, du_src, grav, &
+                                   emissions, rc )
 
 ! !USES:
    implicit NONE
 
 ! !INPUT PARAMETERS:
-   integer, intent(in) :: i1, i2, j1, j2, n_bins           ! grid dimensions
-   real, dimension(:) :: radius, sfrac                      ! particle radius [m]
-   real, pointer, dimension(:,:) :: fraclake, gwettop, oro, u10m, v10m, du_src
-   real :: Ch_DU, grav
+!   integer, intent(in) :: i1, i2, j1, j2, n_bins            ! grid dimensions
+!   real, dimension(:) :: radius, sfrac                      ! particle radius [m]
+!   real, pointer, dimension(:,:) :: fraclake, gwettop, oro, u10m, v10m, du_src
+!   real :: Ch_DU, grav
+
+   real, dimension(:), intent(in) :: radius, sfrac                      ! particle radius [m]
+   real, pointer, dimension(:,:), intent(in) :: fraclake, gwettop, oro, u10m, v10m, du_src
+   real, intent(in) :: Ch_DU, grav
 
 ! !OUTPUT PARAMETERS:
-!   real, intent(inout) ::  emissions(i1:i2, j1:j2, n_bins)    ! Local emission
-   real  ::  emissions(i1:i2, j1:j2, n_bins)    ! Local emission
-
-   integer, intent(out) :: rc                                 ! Error return code:
+!   real  ::  emissions(i1:i2, j1:j2, n_bins)    ! Local emission
+   real, intent(out)  ::  emissions(:,:,:)    ! Local emission
+   integer, intent(out) :: rc                   ! Error return code:
 
 
 ! !DESCRIPTION: Computes the dust emissions for one time step
 !
 ! !REVISION HISTORY:
 !
-! 11Feb202 E.Sherman - First attempt at refactor
+! 11Feb2020 E.Sherman - First attempt at refactor
 !
 !EOP
 !-------------------------------------------------------------------------
 
 ! !Local Variables
-   integer  ::  i, j, e
+   integer         ::  i, j, e
    real, parameter ::  air_dens = 1.25  ! Air density = 1.25 kg m-3
    real, parameter ::  soil_density  = 2650.  ! km m-3
    real            ::  diameter         ! dust effective diameter [m]
    real            ::  u_thresh0
    real            ::  u_thresh
    real            ::  w10m
-
+   integer         ::  i1, i2, j1, j2, n_bins
+   integer         ::  dims(2)
 !   _UNUSED_DUMMY(km)
-
-!print*,'DustEmissionsGOCART2G BEGIN'
 
 !  Initialize local variables
 !  --------------------------
    emissions(:,:,:) = 0.
+   rc = 824
+
+!  Get dimensions
+!  ---------------
+   n_bins = size(radius)
+   dims = shape(u10m)
+   i1 = 1; j1 = 1
+   i2 = dims(1); j2 = dims(2)
 
 !  Calculate the threshold velocity of wind erosion [m/s] for each radius
 !  for a dry soil, as in Marticorena et al. [1997].
 !  The parameterization includes the air density which is assumed 
 !  = 1.25 kg m-3 to speed the calculation.  The error in air density is
 !  small compared to errors in other parameters.
-
-
-!print*,'DustEmissionsGOCART2G i1 = ', i1
-!print*,'DustEmissionsGOCART2G i2 = ', i2
-!print*,'DustEmissionsGOCART2G j1 = ', j1
-!print*,'DustEmissionsGOCART2G j2 = ', j2
-!print*,'DustEmissionsGOCART2G shape(gwettop) = ',shape(gwettop)
-!print*,'DustEmissionsGOCART2G shape(u10m)    = ',shape(u10m)
-!print*,'DustEmissionsGOCART2G shape(v10m)    = ',shape(v10m)
-
-!print*,'DustEmissionsGOCART2G Ch_DU  = ', Ch_DU
-!print*,'DustEmissionsGOCART2G radius  = ', radius
-!print*,'DustEmissionsGOCART2G n_bins  = ', n_bins
 
 
   do e = 1, n_bins
@@ -139,20 +138,117 @@ CONTAINS
 
      end do   ! i
     end do    ! j
-    emissions(:,:,e) = emissions(:,:,e) * Ch_DU * sfrac(e) * du_src
+    emissions(:,:,e) =  Ch_DU * sfrac(e) * du_src *  emissions(:,:,e)
  end do ! e
 
-!print*,'DustEmissionsGOCART2G diameter = ', diameter
-!print*,'DustEmissionsGOCART2G u_thresh0 = ', u_thresh0
-!print*,'DustEmissionsGOCART2G w10m = ', w10m
-
    rc=0
-!print*,'DustEmissionsGOCART2G sum(emissions) = ',sum(emissions)
-!print*,'DustEmissionsGOCART2G END'
 
    end subroutine DustEmissionGOCART2G
 
+!------------------------------------------------------------------------
+!BOP
+! !IROUTINE: DistributePointEmissions
+
+! !INTERFACE:
+
+   subroutine DistributePointEmission(km, delp, rhoa, z_bot, z_top, &
+                                      GRAV, emissions_point, &
+                                      point_column_emissions, rc)
+
+! !USES:
+   implicit NONE
+
+! !INPUT PARAMETERS:
+   integer, intent(in)             :: km
+   real, dimension(:), intent(in)  :: delp, rhoa
+   real, intent(in)                :: z_bot, z_top
+   real,               intent(in)  :: GRAV, emissions_point
+
+
+! !OUTPUT PARAMETERS:
+   real, dimension(:), intent(out) ::  point_column_emissions
+
+   integer, intent(out) :: rc                                 ! Error return code:
+
+
+! !DESCRIPTION: Distributes piont emissions
+!
+! !REVISION HISTORY:
+! ??? P. Colarco
+! 16March2020 E.Sherman - Moved from original DU_GridCompMod.F90 and generalized
+!
+!EOP
+!-------------------------------------------------------------------------
+
+! !Local Variables
+    integer :: k
+    integer :: k_bot, k_top
+    real    :: z_
+    real, dimension(km) :: z, dz, w_
 
 
 
-   end module GriddedEmission
+!   find level height
+    z = 0.0
+    z_= 0.0
+
+    do k = km, 1, -1
+       dz(k) = delp(k)/rhoa(k)/grav
+       z_    = z_ + dz(k)
+       z(k)  = z_
+    end do
+
+!   find the bottom level
+    do k = km, 1, -1
+       if (z(k) >= z_bot) then
+           k_bot = k
+           exit
+       end if
+    end do
+
+!   find the top level
+    do k = k_bot, 1, -1
+       if (z(k) >= z_top) then
+           k_top = k
+           exit
+       end if
+    end do
+
+!   find the weights
+    w_ = 0
+
+
+!   if (k_top > k_bot) then
+!       need to bail - something went wrong here
+!   end if
+
+    if (k_bot .eq. k_top) then
+        w_(k_bot) = z_top - z_bot
+    else
+     do k = k_bot, k_top, -1
+        if ((k < k_bot) .and. (k > k_top)) then
+             w_(k) = dz(k)
+        else
+             if (k == k_bot) then
+                 w_(k) = (z(k) - z_bot)
+             end if
+
+             if (k == k_top) then
+                 w_(k) = z_top - (z(k)-dz(k))
+             end if
+        end if
+     end do
+    end if
+
+!   distribute emissions in the vertical
+    point_column_emissions(:) = (w_ / sum(w_)) * emissions_point
+
+    rc = 0
+
+    end subroutine DistributePointEmission
+
+
+
+
+
+ end module GriddedEmission
