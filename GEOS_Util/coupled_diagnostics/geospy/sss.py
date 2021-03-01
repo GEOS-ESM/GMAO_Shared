a00200a1b4274ae6da3729844450d854807c7798
Yury Vikhliaev <yury.v.vikhliaev@nasa.gov>
2021-03-01 17:36:42 -0500

    SSS vs WOA13 plots added

---
+++ GEOS_Util/coupled_diagnostics/geospy/sss.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+'''
+Plots SSS.
+'''
+
+import importlib
+import numpy as np
+import matplotlib.pyplot as pl
+import cmocean
+import cartopy.crs as ccrs
+import xesmf
+import geosdset, plots
+
+def plot_clim(exp, da):
+    var=da.sel(time=slice(*exp.dates))
+    clim=var.groupby('time.season').mean('time')
+
+    cbar_kwargs={'orientation': 'horizontal',
+                 'shrink': 0.8,
+                 'label': '$^0C$'}
+    
+    fill_opts={'cmap': cmocean.cm.haline, 
+              'levels': np.arange(32.0,38.1,0.4),
+               'cbar_kwargs': cbar_kwargs
+    }
+
+    contour_opts={'levels': np.arange(32.0,38.1,0.8),
+                  'colors': 'black'
+    }
+
+    projection=ccrs.PlateCarree(central_longitude=210.)
+    plotmap=plots.PlotMap(projection=projection, fill_opts=fill_opts, contour_opts=contour_opts)
+
+    pl.figure(1); pl.clf() 
+    ax=plotmap.contour(clim.sel(season='DJF'))
+    ax.set_title('SSS, DJF')
+    pl.savefig(exp.plot_path+'/sss_djf.png')
+    
+    pl.figure(2); pl.clf()
+    ax=plotmap.contour(clim.sel(season='JJA'))
+    ax.set_title('SSS, JJA')
+    pl.savefig(exp.plot_path+'/sss_jja.png')
+
+    pl.figure(3); pl.clf()
+    ax=plotmap.contour(clim.mean('season'))
+    ax.set_title('SSS, Annual Mean')
+    pl.savefig(exp.plot_path+'/sss_am.png')
+    pl.show()
+
+def plot_diff(exp, da1, da2, ftype='dif'):
+    clim1=da1.groupby('time.season').mean('time')
+    clim2=da2.groupby('time.season').mean('time')
+    rr=xesmf.Regridder(da2,da1,'bilinear',periodic=True)
+    dif=clim1-rr(clim2)
+
+    cbar_kwargs={'orientation': 'horizontal',
+                 'shrink': 0.8,
+                 'label': '$^0C$'}
+    
+    fill_opts={'cmap': cmocean.cm.diff, 
+               'levels': np.arange(-5.,5.1,0.5),
+               'cbar_kwargs': cbar_kwargs
+    }
+
+    contour_opts={'levels': np.arange(-5.,5.1,1.0),
+                  'colors': 'black'
+    }
+
+    projection=ccrs.PlateCarree(central_longitude=210.)
+    plotmap=plots.PlotMap(projection=projection, fill_opts=fill_opts, contour_opts=contour_opts)
+
+    pl.figure(1); pl.clf() 
+    ax=plotmap.contour(dif.sel(season='DJF'))
+    ax.set_title('SSS-'+ftype+', DJF')
+    pl.savefig(exp.plot_path+'/sss-'+ftype+'_djf.png')
+    
+    pl.figure(2); pl.clf()
+    ax=plotmap.contour(dif.sel(season='JJA'))
+    ax.set_title('SSS-'+ftype+', JJA')
+    pl.savefig(exp.plot_path+'/sss-'+ftype+'_jja.png')
+
+    pl.figure(3); pl.clf()
+    ax=plotmap.contour(dif.mean('season'))
+    ax.set_title('SSS-'+ftype+', Annual Mean')
+    pl.savefig(exp.plot_path+'/sss-'+ftype+'_am.png')
+    pl.show()
+
+    rr.clean_weight_file()
+
+def mkplots(exps, dsets):
+
+    varname='SS'
+    da=dsets[0][varname]
+    plot_clim(exps[0] ,da)
+
+    for ds in dsets[1:]:
+        da1=ds[varname]
+        plot_diff(exps[0], da, da1, ftype='dif')
+        
+    obs=['woa13']
+    for name in obs:
+        obsvarname='s_an'
+        da1=importlib.import_module('verification.'+name).ds_s[obsvarname].sel(depth=slice(0.,10.)).mean('depth')
+        plot_diff(exps[0], da, da1, ftype='obs')
+
+if __name__=='__main__':
+    import sys
+    exps=geosdset.load_exps(sys.argv[1])
+    dsets=geosdset.load_collection(exps,'geosgcm_ocn2d')
+    mkplots(exps,dsets)
+    
---
+++ GEOS_Util/coupled_diagnostics/geospy/verification/woa13/__init__.py
@@ -0,0 +1,22 @@
+import os
+import cftime
+import xarray as xr
+
+__all_=['ds_t','ds_s','ds_rho']
+
+oceanval=os.environ.get('OCEANVAL',
+                        '/discover/nobackup/projects/gmao/oceanval/verification')
+
+mo=[str(i).zfill(2) for i in range(1,13)]
+
+flist=[oceanval+'/WOA13/'+'woa13_decav_t'+mm+'_01v2.nc' for mm in mo]
+ds_t=xr.open_mfdataset(flist,decode_times=False)
+ds_t=ds_t.assign_coords({'time': cftime.num2date(ds_t.time,ds_t.time.units,calendar='360_day')})
+
+flist=[oceanval+'/WOA13/'+'woa13_decav_s'+mm+'_01v2.nc' for mm in mo]
+ds_s=xr.open_mfdataset(flist,decode_times=False)
+ds_s=ds_s.assign_coords({'time': cftime.num2date(ds_s.time,ds_s.time.units,calendar='360_day')})
+
+flist=[oceanval+'/WOA13/'+'woa13_decav_I'+mm+'_01.nc' for mm in mo]
+ds_rho=xr.open_mfdataset(flist,decode_times=False)
+ds_rho=ds_rho.assign_coords({'time': cftime.num2date(ds_rho.time,ds_rho.time.units,calendar='360_day')})
