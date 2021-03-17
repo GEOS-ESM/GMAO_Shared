8f5998afea1bed334828ff15a15f080bf430260e
Yury Vikhliaev <yury.v.vikhliaev@nasa.gov>
2021-03-17 15:08:29 -0400

    Depth profiles updates.

--- GEOS_Util/coupled_diagnostics/geospy/temp_mapl.py
+++ GEOS_Util/coupled_diagnostics/geospy/temp_mapl.py
@@ -9,13 +9,16 @@ import numpy as np
 import matplotlib.pyplot as pl
 import cmocean
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
-import geosdset, plots
+import geosdset, plots, utils
 
-def plot_clim(exp, da):
-    var=da.sel(time=slice(*exp.dates)).mean('time')
+def plot_clim(exp, ds):
+    varname='T'
+    TFREEZE=273.16
+    var=ds[varname].sel(time=slice(*exp.dates)).mean('time')-TFREEZE
+    var=var.assign_coords({'lev': -var.lev})
+    mask=1.0-np.isnan(var)
 
     cbar_kwargs={'label': '$^0C$',
-                 'extend': 'both'
     }
     
     fill_opts={'yincrease': False,
@@ -27,16 +30,17 @@ def plot_clim(exp, da):
                   'colors': 'black',
                   'yincrease': False}
 
-    '''
-    Note: to do proper spatiall average of 3d fields, 
-    3d metrics with 3d land mask are needed. 
-    '''
-    
     plot=plots.Plot2d(fill_opts=fill_opts, contour_opts=contour_opts)
 
+    '''
+    Plot zonal mean.
+    '''
+
     pl.figure(1)
-    plot_var=var.sel(lev=slice(0.0,3000.0),lat=slice(-85.0,60.0)).mean('lon')
+    wght=mask*ds.dx
+    plot_var=utils.average(var,'lon',wght).sel(lat=slice(-85.0,60.0))
     ax=plot.contour(plot_var)
+    ax.set_ylim(1500.0,0.0)
     ax.set_title('T, zonal mean')
     ax.set_xlabel('latitude')
     ax.set_ylabel('depth')
@@ -44,9 +48,15 @@ def plot_clim(exp, da):
     ax.grid()
     pl.savefig(exp.plot_path+'/T_lat_depth.png')
 
+    '''
+    Plot equatoral profile (-2S -- 2N).
+    '''
+
     pl.figure(2)
-    plot_var=var.sel(lat=slice(-2.1,2.1),lev=slice(0.0,500.0)).mean('lat')
+    wght=mask*ds.dy
+    plot_var=utils.average(var.sel(lat=slice(-2.1,2.1)),'lat',wght.sel(lat=slice(-2.1,2.1)))
     ax=plot.contour(plot_var)
+    ax.set_ylim(500.0,0.0)
     ax.set_title('T, equatorial')
     ax.set_xlabel('longitude')
     ax.set_ylabel('depth')
@@ -55,27 +65,59 @@ def plot_clim(exp, da):
     pl.savefig(exp.plot_path+'/T_eq_depth.png')
 
     pl.show()
-
+    
 def plot_diff(exp, ds1, ds2, type='dif'):
-    pass
-
-def mkplots(exps, dsets):
     varname='T'
     TFREEZE=273.16
-    da=dsets[0][varname]; da-=TFREEZE
-    da=da.assign_coords({'lev': -da.lev})
-    plot_clim(exps[0] ,da)
+    var1=ds1[varname].sel(time=slice(*exp.dates)).mean('time')-TFREEZE
+    var1=var1.assign_coords({'lev': -var1.lev})
+    mask=1.0-np.isnan(var)
+
+    if type=='dif':
+        var2=ds2[varname].sel(time=slice(*exp.dates)).mean('time')-TFREEZE
+        var2=var1.assign_coords({'lev': -var1.lev})
+    elif type=='obs':
+        varname='t_an'
+        var2=ds2[varname].sel(time=slice(*exp.dates)).mean('time')
+    else:
+        print('Wrong plot type. Type should be eighter "dif" or "obs"')
+
+    cbar_kwargs={'label': '$^0C$',
+    }
+    
+    fill_opts={'yincrease': False,
+               'levels': np.arange(-2.0,33.0,2.0),
+               'cmap': cmocean.cm.thermal,
+               'cbar_kwargs': cbar_kwargs}
+
+    contour_opts={'levels': np.arange(0.0,33.0,4.0),
+                  'colors': 'black',
+                  'yincrease': False}
+
+    plot=plots.Plot2d(fill_opts=fill_opts, contour_opts=contour_opts)
+
+    '''
+    Plot zonal mean.
+    '''
+
+    wght=mask*ds1.dx
+    prof1=utils.average(var1,'lon',wght)
+    if type=='dif':
+        wght=mask*ds2.dx
+        prof2=utils.average(var2,'lon',wght)
+    elif type=='obs':
+        prof2=var2.mean('lon')
+
+def mkplots(exps, dsets):
+    plot_clim(exps[0], dsets[0])
 
     for ds in dsets[1:]:
-        da1=ds[varname]; da1-=TFREEZE
-        da1=da1.assign_coords({'lev': -da1.lev})
-        plot_diff(exps[0], da, da1, type='dif')
+        plot_diff(exps[0], dsets[0], ds, type='dif')
 
     obs=['woa13']        
     for name in obs:
-        obsvarname='t_an'
-        da1=importlib.import_module('verification.'+name).ds_t[obsvarname]
-        plot_diff(exps[0], da, da1, type='obs')
+        ds=importlib.import_module('verification.'+name).ds_t[obsvarname]
+        plot_diff(exps[0], dsets[0], ds, type='obs')
 
 if __name__=='__main__':
     import sys
