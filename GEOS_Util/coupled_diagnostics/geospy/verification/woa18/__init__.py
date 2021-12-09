1b1a6b15dc82ea93412c2470ce1cbfd7735cb0fc
Yury Vikhliaev <yury.v.vikhliaev@nasa.gov>
2021-12-09 17:06:19 -0500

    Use string interpolation instead of concatenation.

--- GEOS_Util/coupled_diagnostics/geospy/verification/woa18/__init__.py
+++ GEOS_Util/coupled_diagnostics/geospy/verification/woa18/__init__.py
@@ -2,17 +2,17 @@ import os
 import cftime
 import xarray as xr
 
-__all_=['ds_t','ds_s','ds_rho']
+__all_=['ds_t','ds_s']
 
 oceanval=os.environ.get('OCEANVAL',
                         '/discover/nobackup/projects/gmao/oceanval/verification')
 
 mo=[str(i).zfill(2) for i in range(1,13)]
 
-flist=[oceanval+'/WOA18/'+'woa18_decav_t'+mm+'_04.nc' for mm in mo]
+flist=[f'{oceanval}/WOA18/woa18_decav_t{mm}_04.nc' for mm in mo]
 ds_t=xr.open_mfdataset(flist,decode_times=False)
 ds_t.coords['time']=cftime.num2date(ds_t.time,ds_t.time.units,calendar='360_day')
 
-flist=[oceanval+'/WOA18/'+'woa18_decav_s'+mm+'_04.nc' for mm in mo]
+flist=[f'{oceanval}/WOA18/woa18_decav_s{mm}_04.nc' for mm in mo]
 ds_s=xr.open_mfdataset(flist,decode_times=False)
 ds_s.coords['time']=cftime.num2date(ds_s.time,ds_s.time.units,calendar='360_day')
