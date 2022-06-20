b714faf96565bc35ef0d79b95c655e54ed04e365
Weiyuan Jiang <weiyuan.jiang@nasa.gov>
2022-06-20 12:33:00 -0400

    initial check in c24Toc12 case

--- GEOS_Util/post/tests/test_remap_restarts.py
+++ GEOS_Util/post/tests/test_remap_restarts.py
@@ -10,6 +10,7 @@ import sys, getopt
 import ruamel.yaml
 import questionary
 import glob
+import subprocess as sp
 import remap_restarts 
 from remap_questions import get_config_from_questionary
 from remap_params import *
@@ -20,34 +21,56 @@ from remap_catchANDcn  import *
 
 def compare(base, result):
   #1) comparing nc4
-  bases   =  glob.glob(base + '/*_rst*.nc4')
-  results =  glob.glob( result + '/*_rst*.nc4')
-  assert ( len(bases) == len(results), " number of restart out should be the same")
-
+  bases   =  glob.glob(base + '/*_rst*.nc4*')
+  results =  glob.glob( result + '/*_rst*.nc4*')
+  if (len(bases) != len(results)) :
+     print(len(bases), len(results))
+     print (" number of restart out should be the same")
+     return False
   for f in bases:
-     bname = os.basename(f)
-     core = banme.split('_rst')[0].split('.')[-1]+'_rst'
+     bname = os.path.basename(f)
+     core = bname.split('_rst')[0].split('.')[-1]+'_rst'
      for r in results:
         if core in r :
           cmd = 'nccmp -dmgfs '+ f + ' ' + r
-          p = subprocess.Popen(shlex.split(cmd), universal_newlines=True, stdout=sp.PIPE, stderr=sp.PIPE)
+          print(cmd)
+          p = sp.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
+          (out, err) = p.communicate()
           rc = p.wait()
-          if not rc : 
+          out = out.decode().split()
+          if "identical." in out :
+             print('identical')
+          else: 
             print ( f + ' is different from ' + r)
             return False
   return True
+def test_remap(config_yaml):
+  upper = upperair(config_yaml)
+  upper.remap()
+  lls  = lake_landice_saltwater(config_yaml)
+  lls.remap()
+  catch  = catchANDcn(config_yaml)
+  catch.remap()
+  ana = analysis(config_yaml)
+  ana.remap()
+
 
 if __name__ == '__main__' :
+
   # test c24_to_c12
   base_c12 = '/gpfsm/dnb32/wjiang/pl_c24Tc12'
   yaml = ruamel.yaml.YAML()
   stream =''
-  c24toc12 = 'c24Toc12.yaml'
-  with  open(c24toc12, 'r') as f:
+  config_yaml = 'c24Toc12.yaml'
+  with  open(config_yaml, 'r') as f:
      stream = f.read()
   results_c12 = yaml.load(stream)['output']['shared']['out_dir']
-  remap_restarts.main -c c24toc12
-  
-  rc = compare(base_c12, results_c12)
-  assert( rc, 'failed to compare ' + base_c12)
 
+  #test_remap(config_yaml)
+ 
+  rc = compare(base_c12, results_c12)
+  if (not rc) :
+    print ("failed in c24Toc12")
+  else :
+    print ("c24Toc12 tests is successful")
+ 
